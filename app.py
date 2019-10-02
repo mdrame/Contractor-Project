f5500c1100ad69ac605885420b99e40eb752b049
mohammed <mdrame113@gmail.com>
2019-10-01 17:28:11 -0700

    set up app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+# welcome to my word of code ♥️
+
+@app.route('/') #index
+def dashBorad():
+
+    message = ' Welcome to the Make Store '
+
+    return(message)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
