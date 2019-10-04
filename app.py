6b70639c54a3c02ea2e3b5e81a4d3f009dd58ebb
mohammed <mdrame113@gmail.com>
2019-10-03 17:21:14 -0700

    basic wireframe

--- app.py
+++ app.py
@@ -5,12 +5,20 @@ app = Flask(__name__)
 
 # welcome to my word of code ♥️
 
+# products / a list individual  dictionarys 
+products = [ {"name": "Red rose", "description": "Red  & organic"},
+             {"name": "Mix flower ", "description": "Mix flower "},
+             {"name": "Sun Flower ", "description": " Sun looking like"},
+             {"name": "Pink Rose", "description": "Fresh and pike flower rose"},
+             {"name": "Heart", "description": " Origin "} ]
+
+
+
 @app.route('/') #index
 def dashBorad():
 
-    message = 'Welcome to the Pure | 🌹'
 
-    return render_template("home.html", message=message)
+    return render_template("home.html", products=products)
 
 
 if __name__ == '__main__':
