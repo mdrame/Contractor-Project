151bfa9b7961c838979d1079d87032a537fcd646
mohammed <mdrame113@gmail.com>
2019-10-03 22:38:05 -0700

    formpage  and view page added

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ app = Flask(__name__)
 # welcome to my word of code ♥️
 
 # products / a list individual  dictionarys 
-products = [ {"name": "Red rose", "description": "Red  & organic"},
-             {"name": "Mix flower ", "description": "Mix flower "},
-             {"name": "Sun Flower ", "description": " Sun looking like"},
-             {"name": "Pink Rose", "description": "Fresh and pike flower rose"},
-             {"name": "Heart", "description": " Origin "} ]
+products = [ {"name": "Red rose", "price": " $5.00 "},
+             {"name": "Mix flower ", "price": "$10.00 "},
+             {"name": "Sun Flower ", "price": "$20.00"},
+             {"name": "Pink Rose", "price": "$15.00"},
+             {"name": "Heart", "price": "$12.00"} ]
 
 
 
@@ -18,7 +18,17 @@ products = [ {"name": "Red rose", "description": "Red  & organic"},
 def dashBorad():
 
 
-    return render_template("home.html", products=products)
+    return render_template("base.html")
+
+@app.route('/listingForm')
+def createListing():
+
+    return render_template('listingForm.html')
+
+@app.route('/home')
+def viewItem():
+
+    return render_template('home.html', products=products)
 
 
 if __name__ == '__main__':
