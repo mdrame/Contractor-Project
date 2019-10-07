77d2b2f2bd888b86e0daf90c3ba5469417c731fe
mohammed <mdrame113@gmail.com>
2019-10-07 16:28:37 -0700

    user can create listing:

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
+
 
 
 app = Flask(__name__)
@@ -20,20 +21,29 @@ listings = db.listings
 
 
 @app.route('/') #index
-def dashBorad():
+def dashBoard():
 
 
-    return render_template("base.html")
+    return render_template("base.html", listings=listings.find())
 
 @app.route('/listingForm')
-def createListing():
+def listing():
 
+    
     return render_template('listingForm.html')
 
-@app.route('/home')
-def viewItem():
+@app.route('/listings', methods=['POST'])
+def listing_submit():
 
-    return render_template('home.html', listings=listings.find())
+    # testing to see if infos from form was sent correctly
+    print(request.form.to_dict())
+
+    itemList = {'title': request.form.get('itemName'),
+                'price': request.form.get("itemPrice" + '$')}
+
+    listings.insert_one(itemList)
+    return redirect(url_for('dashBoard'))
+
 
 
 if __name__ == '__main__':
