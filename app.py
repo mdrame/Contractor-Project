32db07816631793db9f367ef6d7cef6b0c6e4dcf
mohammed <mdrame113@gmail.com>
2019-10-07 17:11:31 -0700

    user can see single playlist

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
-
+# this under help change Mongodb _id to an object 
+from bson.objectid import ObjectId
 
 
 app = Flask(__name__)
@@ -32,17 +33,28 @@ def listing():
     
     return render_template('listingForm.html')
 
+# this function / route is appendig the listing to my local mongo db
 @app.route('/listings', methods=['POST'])
 def listing_submit():
+
+    ''' create a dictionary of listing 
+    form name and price & inseart it into listings db '''
 
     # testing to see if infos from form was sent correctly
     print(request.form.to_dict())
 
     itemList = {'title': request.form.get('itemName'),
-                'price': request.form.get("itemPrice" + '$')}
+                'price': request.form.get("itemPrice")}
 
-    listings.insert_one(itemList)
-    return redirect(url_for('dashBoard'))
+    listing_id = listings.insert_one(itemList).inserted_id
+    return redirect(url_for('viewProduct', listing_id=listing_id))
+
+# route to view individual listing | Read in CRUD
+@app.route('/<listing_id>')
+def viewProduct(listing_id):
+
+    listing = listings.find_one({'_id': ObjectId(listing_id)})
+    return render_template('viewProduct.html', listing=listing)
 
 
 
